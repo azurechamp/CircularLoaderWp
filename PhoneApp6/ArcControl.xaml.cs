32d6999b009fae1b32d36e3e4bf46a0061ec24d6
Muhammad92 <saadmehmood21@gmail.com>
2015-03-31 22:38:15 +0500

    Main Commit

---
+++ PhoneApp6/ArcControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+
+namespace PhoneApp6
+{
+    public partial class ArcControl : UserControl
+    {
+        public ArcControl()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        public int Percentage
+        {
+            get { return (int)GetValue(PercentageProperty); }
+            set 
+            { 
+                SetValue(PercentageProperty, value);
+                Arc_to_Rotate.EndAngle = value * 3.6;
+            }
+        }
+
+
+        // Using a DependencyProperty as the backing store for Percentage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PercentageProperty =
+            DependencyProperty.Register("Percentage", typeof(int), typeof(ArcControl), new PropertyMetadata(0));
+
+
+
+
+
+
+
+
+        public Brush ArcColor
+        {
+            get { return (Brush)GetValue(ArcColorProperty); }
+            set { SetValue(ArcColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ArcColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ArcColorProperty =
+            DependencyProperty.Register("ArcColor", typeof(Brush), typeof(ArcControl), null);
+
+        
+        
+
+
+        
+
+
+        
+        
+		
+    }
+}
---
+++ PhoneApp6/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneApp6.Resources;
+
+namespace PhoneApp6
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
